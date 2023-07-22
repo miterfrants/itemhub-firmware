77fe3bc08bc9a7f7ddbde03e4150c73d0acfd165
Peter Huang <miterfrnats@gmail.com>
2023-07-23 00:18:50 +0800

    fix: 修正 node mcu 連線帳號密碼中間少了逗號

--- node-mcu/http/node-mcu.ino
+++ node-mcu/http/node-mcu.ino
@@ -39,7 +39,7 @@ void setup()
   Serial.print("Connecting to ");
   Serial.println(WIFI_SSID);
   WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFI_SSID WIFI_PWD);
+  WiFi.begin(WIFI_SSID, WIFI_PWD);
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(1000);
